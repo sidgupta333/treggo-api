bd80b76f53195fbfa91d06315793c7735b8b78b3
Siddhant Gupta <siddhantgupta89@gmail.com>
2019-11-06 20:11:21 +0530

    First commit

---
+++ src/main/java/com/api/treggo/TreggoApplication.java
@@ -0,0 +1,13 @@
+package com.api.treggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreggoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/treggo/TreggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.treggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
