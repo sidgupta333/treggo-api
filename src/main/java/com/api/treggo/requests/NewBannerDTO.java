bb7abb9ca3e2bdc2760333f243cdb96ec7a0d8a9
Siddhant Gupta <siddhantgupta89@gmail.com>
2019-11-27 20:43:16 +0530

    Update NewBannerDTO.java

--- src/main/java/com/api/treggo/requests/NewBannerDTO.java
+++ src/main/java/com/api/treggo/requests/NewBannerDTO.java
@@ -1,6 +1,5 @@
 package com.api.treggo.requests;
 
-import java.time.LocalDate;
 
 import com.api.treggo.enums.YesNo;
 
@@ -9,14 +8,14 @@ public class NewBannerDTO {
 	private Long banner_id;
 	private Long img_id;
 	private YesNo is_available;
-	private LocalDate start_date;
+	private String start_date;
 	
 	public NewBannerDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public NewBannerDTO(Long banner_id, Long img_id, YesNo is_available, LocalDate start_date
+	public NewBannerDTO(Long banner_id, Long img_id, YesNo is_available, String start_date
 			) {
 		super();
 		this.banner_id = banner_id;
@@ -49,11 +48,11 @@ public class NewBannerDTO {
 		this.is_available = is_available;
 	}
 
-	public LocalDate getStart_date() {
+	public String getStart_date() {
 		return start_date;
 	}
 
-	public void setStart_date(LocalDate start_date) {
+	public void setStart_date(String start_date) {
 		this.start_date = start_date;
 	}	
 }
